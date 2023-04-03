d10ae7528a7f999a96b70fbc583306b9adf6fa02
Uallessonivo <uallessons@gmail.com>
2023-04-03 14:01:46 -0300

    updating

--- CSVParser/Program.cs
+++ CSVParser/Program.cs
@@ -1,32 +1,79 @@
-﻿using ExcelDataReader;
+﻿using System.Data;
+using System.Globalization;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using ExcelDataReader;
 
 namespace CSVParser;
+
+public class CsvRecords
+{
+    public long Serial { get; set; }
+    public long Cpf { get; set; }
+    public string? Valor { get; set; }
+    public string? Colaborador { get; set; }
+}
+
+public class CsvHeader : ClassMap<CsvRecords>
+{
+    public CsvHeader()
+    {
+        Map(c => c.Serial).Name("Numero de Serie");
+        Map(c => c.Cpf).Name("CPF");
+        Map(c => c.Valor).Name("Valor da Carga");
+        Map(c => c.Colaborador).Name("Observacao");
+    }
+}
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Files", "cartoes.xlsx");
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Files", "cartoes.xlsx");
 
-        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-        using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+        var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+
+        var reader = ExcelReaderFactory.CreateReader(stream);
+
+        var dataSet = reader.AsDataSet();
+
+        var records = new List<CsvRecords>();
+
+        foreach (var row in dataSet.Tables[0].Rows.Cast<DataRow>().Skip(1))
         {
-            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            if (string.IsNullOrEmpty(row[1]?.ToString()) || string.IsNullOrEmpty(row[2]?.ToString()))
             {
-                do
-                {
-                    while (reader.Read())
-                    {
-                        Console.WriteLine(reader.GetValue(1));
-                        Console.WriteLine(reader.GetValue(2));
-                        Console.WriteLine(reader.GetValue(3));
-                        Console.WriteLine(reader.GetValue(4));
-                        Console.WriteLine(reader.GetValue(5));
-                        Console.WriteLine(reader.GetValue(6));
-                    }
-                } while (reader.NextResult());
+                continue;
             }
+
+            records.Add(new CsvRecords
+            {
+                Serial = long.Parse((string)row[1]),
+                Cpf = long.Parse(row[2].ToString().Replace(".", "").Replace("-", "")),
+                Valor = "",
+                Colaborador = row[3].ToString()
+            });
         }
+
+        Console.WriteLine("Digite o nome do arquivo a ser gerado: ");
+        var filename = Console.ReadLine();
+
+        var outputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Files",
+            $"{DateTime.Today:yyyMMdd}-{filename}.csv");
+
+        using var writer = new StreamWriter(outputFilePath);
+
+        using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";"
+        });
+
+        csv.Context.RegisterClassMap<CsvHeader>();
+        csv.WriteHeader<CsvRecords>();
+        csv.WriteRecords(new[] {new CsvRecords()});
+        csv.WriteRecords(records);
     }
 }
